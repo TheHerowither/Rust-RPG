8ca9b5a6e8063e932cef2326bb838044004001f6
TheHerowither <lucland278@gmail.com>
2023-05-17 20:24:02 +0200

    Added a new const with total amount of id's

--- src/shell/interactions.rs
+++ src/shell/interactions.rs
@@ -4,7 +4,9 @@ use main_funcs::*;
 use rand::Rng;
 
 pub const ITEMS : [Item<'static>; 3] = [Item{name : "Fires edge", id : 0, descr : "A fiery blade, that pierces its foes\nwith fiery slashes", stats : [1.3, 0.2, 0.0, 0.0], damage_type : "Fire"}, Item{name : "Stonk", id : 1, descr : "A pickaxe that produces STONKS", stats : [0.0, 0.0, 1.0, 3.2], damage_type : ""}, Item{name : "Herring the red", id : 2, descr : "A red herring", stats : [0.0, 0.0, 0.0, 0.0], damage_type : ""}];
+pub const TOTALITEMID : i32 = ITEMS.len() as i32;
 pub const ARMOURS : [Armour<'static>; 1] = [Armour{name : "Firestone Helmet", id : 0, descr : "A helmet, that looks\nto be made of molten rock", armour : 1.0, stats : [0.3, 0.6], damage_resistance : "Fire", damage_resistance_addition : 0.7}];
+pub const TOTALARMOURSID : i32 = ARMOURS.len() as i32;
 
 pub fn get_random_item() -> String {
     let mut rng = rand::thread_rng();
