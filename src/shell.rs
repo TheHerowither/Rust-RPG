e872c2bdebe72910130f1b02cb93badb1ed7e82a
TheHerowither <lucland278@gmail.com>
2023-05-18 01:49:07 +0200

    Fixed up the shell

--- src/shell.rs
+++ src/shell.rs
@@ -29,28 +29,48 @@ pub fn debug_loop() {
         //Handle the input:
         if cmd == "exit" {
             break;}
-        if cmd == "rng_item" {
+        else if cmd == "rng_item" {
             println!("{}", get_random_item());}
-        if cmd == "rng_armour" {
+        else if cmd == "rng_armour" {
             println!("{}", get_random_armour());}
-        if split[0] == "get_item_by_id" {
+        else if split[0] == "get_item_by_id" {
             let r = item_to_string(get_item_by_id(split[1].to_string().parse::<i32>().unwrap()));
             println!("{}", r);
         }
-        if split[0] == "get_armour_by_id" {
+        else if split[0] == "get_armour_by_id" {
             let r = armour_to_string(get_armour_by_id(split[1].to_string().parse::<i32>().unwrap()));
             println!("{}", r);
         }
-        if cmd == "?" {
+        else if cmd == "?" {
+            //Print out all of the valid commands, and what they do
             println!("exit - Closes the program");
             println!("rng_item - prints out a random item");
             println!("rng_armour - prints out a random armour");
             println!("get_item_by_id id - prints out the item with the specified id");
             println!("get_armour_by_id id - prints out the armour with the specified id");
-            //TODO: Print out all of the valid commands, and what they do
         }
+        else {
+            println!("Invalid command\nTry writing '?' to see the commands\nRemember: it IS context sensitive");
+        }
+        println!();
     }
 }
 pub fn main_loop() {
-    
+    loop {
+        let cmd : String = input("> ");
+        let split: std::str::Split<&str> = cmd.split(" ");
+        let split: Vec<&str> = split.collect();
+
+        //Handle the input:
+        if split[0] == "exit" {
+            break;}
+        else if split[0] == "?"{
+            //Print out all of the possible commands and what they do
+            println!("exit - Closes the program");
+        }
+        else {
+            println!("Invalid command\nTry writing '?' to see the commands\nRemember: it IS context sensitive");
+        }
+        println!();
+    }
 }
